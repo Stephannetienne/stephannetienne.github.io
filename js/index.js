30f0ce22452f8e7021c716945e67fc1dafffc403
Stephann Etienne <112012498+Stephannetienne@users.noreply.github.com>
2024-07-17 16:52:24 -0400

    lightsbox added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+function displaylightbox(url, options)
+{
+   options.items = { src: url };
+   options.type = 'iframe';
+   $.magnificPopup.open(options);
+}
 document.addEventListener('DOMContentLoaded', function(event)
 {
    var galleryCarousel = new bootstrap.Carousel('#galleryCarousel', {interval: 0, pause: false});
@@ -14,5 +20,4 @@ $(document).ready(function()
       event.preventDefault();
       $('html, body').stop().animate({ scrollTop: $('#wb_form3LayoutGrid1').offset().top-80 }, 600, 'easeInQuad');
    });
-   $('#wb_ImageHotspots1 map').imageMapResize();
 });
